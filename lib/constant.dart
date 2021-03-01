a3ca7855ff6feea1d2bad9154c44a193e2a42c2f
mukxdev <mukxdev@gmail.com>
2021-03-01 04:40:01 +0300

    Added a constant file

---
+++ lib/constant.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const appBarShapeBorder = RoundedRectangleBorder(
+  borderRadius: BorderRadius.only(
+    bottomLeft: Radius.circular(20),
+    bottomRight: Radius.circular(20),
+  ),
+);
