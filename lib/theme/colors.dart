92916e05c6bcbad88bef392981d98c78d566c43a
mukxdev <mukxdev@gmail.com>
2021-03-01 04:37:34 +0300

    Updated the AppColors and AppTheme

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const darkBackground = Color(0xff181818);
-  static const darkGray = Color(0xff2b2d42);
-  static const lightGray = Color(0xff8d99ae);
-  static const darkRed = Color(0xffd90429);
-  static const lightRed = Color(0xffef233c);
-  static const white = Color(0xffedf2f4);
+  static const darkBackground = Color(0xff1C1D22);
+  static const darkGray = Color(0xff25262C);
+  static const lightGray = Color(0xffE8E7EC);
+  static const darkPurple = Color(0xff4A28D1);
+  static const lightPurple = Color(0xff674FFF);
+  static const darkGreen = Color(0xff4A9F80);
+  static const lightGreen = Color(0xff69C3A3);
+  static const darkRed = Color(0xffC83949);
+  static const lightRed = Color(0xffFD7676);
+  static const white = Color(0xffFFFFFF);
+  static const lightNote = Color(0xffFAF6EA);
+  static const darkNote = Color(0xff383838);
 }
--- lib/theme/themes.dart
+++ lib/theme/themes.dart
@@ -1,18 +1,71 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import './colors.dart';
 
 class AppThemes {
   static ThemeData lightTheme = ThemeData(
+    textTheme: GoogleFonts.assistantTextTheme().apply(
+      bodyColor: AppColors.darkBackground,
+      displayColor: AppColors.darkBackground,
+    ),
     brightness: Brightness.light,
     primaryColor: AppColors.lightGray,
-    accentColor: AppColors.lightRed,
+    accentColor: AppColors.lightPurple,
     scaffoldBackgroundColor: AppColors.white,
+    textSelectionHandleColor: AppColors.lightGreen,
+    cursorColor: AppColors.lightGreen,
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: AppColors.lightGray,
+      labelStyle: TextStyle(color: AppColors.lightPurple),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(10),
+        ),
+        borderSide: BorderSide(
+          color: AppColors.lightPurple,
+          width: 2,
+        ),
+      ),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(10),
+        ),
+        borderSide: BorderSide.none,
+      ),
+    ),
   );
 
   static ThemeData darkTheme = ThemeData(
+    textTheme: GoogleFonts.assistantTextTheme().apply(
+      bodyColor: AppColors.white,
+      displayColor: AppColors.white,
+    ),
     brightness: Brightness.dark,
     primaryColor: AppColors.darkGray,
-    accentColor: AppColors.darkRed,
+    accentColor: AppColors.darkPurple,
     scaffoldBackgroundColor: AppColors.darkBackground,
+    textSelectionHandleColor: AppColors.darkGreen,
+    cursorColor: AppColors.darkGreen,
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: AppColors.darkGray,
+      labelStyle: TextStyle(color: AppColors.darkPurple),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(10),
+        ),
+        borderSide: BorderSide(
+          color: AppColors.darkPurple,
+          width: 2,
+        ),
+      ),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.all(
+          Radius.circular(10),
+        ),
+        borderSide: BorderSide.none,
+      ),
+    ),
   );
 }
