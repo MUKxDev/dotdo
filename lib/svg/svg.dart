d736470cef16f99bf3b0e61a112277eb1fb5709c
mukxdev <mukxdev@gmail.com>
2021-03-01 04:35:50 +0300

    Added dotDo logo svg

---
+++ lib/svg/svg.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+final Widget logoSvgLight =
+    SvgPicture.asset('assets/LogoLight.svg', semanticsLabel: '.Do Logo');
+final Widget logoSvgDark =
+    SvgPicture.asset('assets/LogoDark.svg', semanticsLabel: '.Do Logo');
