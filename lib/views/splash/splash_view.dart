8e2f15cb38216e11afcd95bf1f922cb609d93815
mukxdev <mukxdev@gmail.com>
2021-03-01 04:36:22 +0300

    Updated the splash view

--- lib/views/splash/splash_view.dart
+++ lib/views/splash/splash_view.dart
@@ -1,16 +1,49 @@
+import 'package:dotdo/svg/svg.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import 'splash_view_model.dart';
-          
+
 class SplashView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ViewModelBuilder<SplashViewModel>.reactive(
+      onModelReady: (SplashViewModel viewmodel) => viewmodel.handelStartup(),
       builder: (BuildContext context, SplashViewModel viewModel, Widget _) {
         return Scaffold(
-          appBar: AppBar(),
           body: Center(
-            child: Text('Splash View'),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Theme.of(context).brightness == Brightness.light
+                    ? Hero(
+                        tag: '_logoSvgLight',
+                        child: Container(
+                          child: logoSvgLight,
+                          height: 130,
+                          width: 130,
+                        ),
+                      )
+                    : Hero(
+                        tag: '_logoSvgDark',
+                        child: Container(
+                          child: logoSvgDark,
+                          height: 130,
+                          width: 130,
+                        ),
+                      ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    '.Do',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).accentColor,
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         );
       },
--- lib/views/splash/splash_view_model.dart
+++ lib/views/splash/splash_view_model.dart
@@ -1,11 +1,40 @@
+import 'dart:async';
+
+import 'package:dotdo/core/locator.dart';
+import 'package:dotdo/core/router_constants.dart';
 import 'package:logger/logger.dart';
 import 'package:stacked/stacked.dart';
 import 'package:dotdo/core/logger.dart';
+import 'package:stacked_firebase_auth/stacked_firebase_auth.dart';
+import 'package:stacked_services/stacked_services.dart';
 
 class SplashViewModel extends BaseViewModel {
   Logger log;
 
   SplashViewModel() {
     this.log = getLogger(this.runtimeType.toString());
+  }
+
+  final NavigationService _navigationService = locator<NavigationService>();
+
+  Future navigateToLogin() async {
+    _navigationService.pushNamedAndRemoveUntil(loginViewRoute);
+  }
+
+  Future navigateToToday() async {
+    _navigationService.pushNamedAndRemoveUntil(todayViewRoute);
+  }
+
+  Future handelStartup() async {
+    Timer(
+      Duration(seconds: 2),
+      () {
+        if (FirebaseAuthenticationService().hasUser) {
+          navigateToToday();
+        } else {
+          navigateToLogin();
+        }
+      },
+    );
   }
 }
