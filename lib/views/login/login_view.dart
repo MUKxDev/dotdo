5c41b7e83896c3cc8f326043358991bf6b9f81df
mukxdev <mukxdev@gmail.com>
2021-03-01 04:36:45 +0300

    Added a login view

---
+++ lib/views/login/login_view.dart
@@ -0,0 +1,166 @@
+import 'package:dotdo/constant.dart';
+import 'package:dotdo/svg/svg.dart';
+import 'package:dotdo/theme/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+import 'login_view_model.dart';
+
+class LoginView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ViewModelBuilder<LoginViewModel>.reactive(
+      builder: (BuildContext context, LoginViewModel viewModel, Widget _) {
+        // var emailController = useTextEditingController(text: 'hi');
+        return GestureDetector(
+          onTap: () => FocusScope.of(context).unfocus(),
+          child: Scaffold(
+            appBar: AppBar(
+              shape: appBarShapeBorder,
+              title: Text('Sign in'),
+            ),
+            body: Center(
+              child: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Column(
+                  children: [
+                    //* .Do logo svg
+                    Theme.of(context).brightness == Brightness.light
+                        ? Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Hero(
+                              tag: '_logoSvglight',
+                              child: Container(
+                                child: logoSvgLight,
+                                height: 100,
+                                width: 100,
+                              ),
+                            ))
+                        : Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Hero(
+                              tag: '_logoSvgDark',
+                              child: Container(
+                                child: logoSvgDark,
+                                height: 130,
+                                width: 130,
+                              ),
+                            ),
+                          ),
+                    // * email textfield
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20.0, vertical: 8),
+                      child: TextField(
+                        onChanged: viewModel.updateEmail,
+                        style: TextStyle(
+                          color:
+                              Theme.of(context).brightness == Brightness.light
+                                  ? AppColors.darkBackground
+                                  : AppColors.white,
+                        ),
+                        autocorrect: false,
+                        autofocus: false,
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          hintText: 'Enter Email...',
+                        ),
+                      ),
+                    ),
+                    // * password textfield
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20.0, vertical: 8),
+                      child: TextField(
+                        onChanged: viewModel.updatePassword,
+                        style: TextStyle(
+                          color:
+                              Theme.of(context).brightness == Brightness.light
+                                  ? AppColors.darkBackground
+                                  : AppColors.white,
+                        ),
+                        autocorrect: false,
+                        autofocus: false,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                          hintText: 'Enter Password...',
+                        ),
+                      ),
+                    ),
+
+                    // * Sign in button
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20.0, vertical: 8),
+                      child: viewModel.isLoading
+                          ? SizedBox(
+                              child: CircularProgressIndicator(),
+                              height: 60,
+                              width: 60,
+                            )
+                          : Container(
+                              height: 60,
+                              width: double.infinity,
+                              child: FlatButton(
+                                color: Theme.of(context).accentColor,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(20),
+                                ),
+                                onPressed: viewModel.signinWithEmail,
+                                child: Text(
+                                  'Sign in',
+                                  style: TextStyle(
+                                      color: AppColors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18),
+                                ),
+                              ),
+                            ),
+                    ),
+                    // * Or Text
+                    Center(
+                      child: Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          'Or',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                    ),
+                    // * Register button
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20.0, vertical: 8),
+                      child: Container(
+                        height: 60,
+                        width: double.infinity,
+                        child: FlatButton(
+                          color: Theme.of(context).primaryColor,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          onPressed: viewModel.navigateToRegister,
+                          child: Text(
+                            'Register',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+      viewModelBuilder: () => LoginViewModel(),
+    );
+  }
+}
---
+++ lib/views/login/login_view_model.dart
@@ -0,0 +1,86 @@
+import 'package:dotdo/core/locator.dart';
+import 'package:dotdo/core/router_constants.dart';
+import 'package:logger/logger.dart';
+import 'package:stacked/stacked.dart';
+import 'package:dotdo/core/logger.dart';
+import 'package:stacked_firebase_auth/stacked_firebase_auth.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+class LoginViewModel extends BaseViewModel {
+  Logger log;
+  String _email = '';
+  String get email => _email;
+  String _password = '';
+  String get password => _password;
+  bool _isLoading = false;
+  bool get isLoading => _isLoading;
+
+  // AuthService _authService = locator<AuthService>();
+  DialogService _dialogService = locator<DialogService>();
+  SnackbarService _snackbarService = locator<SnackbarService>();
+  NavigationService _navigationService = locator<NavigationService>();
+
+  LoginViewModel() {
+    log = getLogger(this.runtimeType.toString());
+  }
+
+  void updateEmail(String value) {
+    _email = value;
+    notifyListeners();
+  }
+
+  void updatePassword(String value) {
+    _password = value;
+    notifyListeners();
+  }
+
+  void toggleIsLodaing() {
+    _isLoading = !_isLoading;
+    notifyListeners();
+  }
+
+  // ! Make sure which one is better from AuthService or directly
+  // TODO: try putting FirebaseAuthenticationService in the locator to make the third option.
+  // ? This is directly from FirebaseAuthenticationService() package.
+  void signinWithEmail() async {
+    toggleIsLodaing();
+
+    if (_email == '' || _password == '') {
+      _snackbarService.showSnackbar(message: 'Please fill the required fields');
+    } else {
+      toggleIsLodaing();
+      final result = await FirebaseAuthenticationService()
+          .loginWithEmail(email: email, password: password);
+
+      if (result.hasError) {
+        toggleIsLodaing();
+        _dialogService.showDialog(
+          barrierDismissible: true,
+          title: 'Error',
+          description: result.errorMessage,
+        );
+      } else {
+        toggleIsLodaing();
+        print('Auth sign in with email');
+        print('Result uid: ${result.uid}');
+        _navigationService.pushNamedAndRemoveUntil(
+          todayViewRoute,
+          // arguments: {1: result.uid},
+        );
+      }
+    }
+  }
+
+  // ? old sign in from AuthService
+  // void signinWithEmail() {
+  //   _authService.signInWithEmail(email: email, password: password);
+  // }
+
+  // void signupWithEmail() {
+  //   _authService.createNewAccount(email: email, password: password);
+  // }
+
+  void navigateToRegister() {
+    _navigationService.navigateTo(registerViewRoute);
+  }
+}
