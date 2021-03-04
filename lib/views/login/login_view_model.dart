40b8d1d176fe084c02b67c715580d0d895edcbec
mukxdev <mukxdev@gmail.com>
2021-03-05 01:53:15 +0300

    Used a TextEditingController

--- lib/views/login/login_view_model.dart
+++ lib/views/login/login_view_model.dart
@@ -1,5 +1,6 @@
 import 'package:dotdo/core/locator.dart';
 import 'package:dotdo/core/router_constants.dart';
+import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 import 'package:stacked/stacked.dart';
 import 'package:dotdo/core/logger.dart';
@@ -8,47 +9,34 @@ import 'package:stacked_services/stacked_services.dart';
 
 class LoginViewModel extends BaseViewModel {
   Logger log;
-  String _email = '';
-  String get email => _email;
-  String _password = '';
-  String get password => _password;
   bool _isLoading = false;
   bool get isLoading => _isLoading;
 
-  // AuthService _authService = locator<AuthService>();
+  TextEditingController emailController = TextEditingController(text: '');
+  TextEditingController passwordController = TextEditingController(text: '');
+
   DialogService _dialogService = locator<DialogService>();
   SnackbarService _snackbarService = locator<SnackbarService>();
   NavigationService _navigationService = locator<NavigationService>();
+  FirebaseAuthenticationService _authenticationService =
+      locator<FirebaseAuthenticationService>();
 
   LoginViewModel() {
     log = getLogger(this.runtimeType.toString());
   }
-
-  void updateEmail(String value) {
-    _email = value;
-    notifyListeners();
-  }
-
-  void updatePassword(String value) {
-    _password = value;
-    notifyListeners();
-  }
-
   void toggleIsLodaing() {
     _isLoading = !_isLoading;
     notifyListeners();
   }
 
-  // ! Make sure which one is better from AuthService or directly
-  // TODO: try putting FirebaseAuthenticationService in the locator to make the third option.
-  // ? This is directly from FirebaseAuthenticationService() package.
   void signinWithEmail() async {
-    if (_email == '' || _password == '') {
+    if (emailController.text == '' || passwordController.text == '') {
       _snackbarService.showSnackbar(message: 'Please fill the required fields');
     } else {
       toggleIsLodaing();
-      final result = await FirebaseAuthenticationService()
-          .loginWithEmail(email: email, password: password);
+
+      final result = await _authenticationService.loginWithEmail(
+          email: emailController.text, password: passwordController.text);
 
       if (result.hasError) {
         toggleIsLodaing();
@@ -61,24 +49,9 @@ class LoginViewModel extends BaseViewModel {
         toggleIsLodaing();
         print('Auth sign in with email');
         print('Result uid: ${result.uid}');
-        _navigationService.pushNamedAndRemoveUntil(
-          todayViewRoute,
-          // arguments: {1: result.uid},
-        );
+
+        _navigationService.pushNamedAndRemoveUntil(homeViewRoute);
       }
     }
-  }
-
-  // ? old sign in from AuthService
-  // void signinWithEmail() {
-  //   _authService.signInWithEmail(email: email, password: password);
-  // }
-
-  // void signupWithEmail() {
-  //   _authService.createNewAccount(email: email, password: password);
-  // }
-
-  void navigateToRegister() {
-    _navigationService.navigateTo(registerViewRoute);
   }
 }
--- lib/views/register/register_view_model.dart
+++ lib/views/register/register_view_model.dart
@@ -1,6 +1,5 @@
 import 'package:dotdo/core/locator.dart';
 import 'package:dotdo/core/router_constants.dart';
-import 'package:dotdo/core/services/authService.dart';
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
 import 'package:stacked/stacked.dart';
@@ -10,39 +9,23 @@ import 'package:stacked_services/stacked_services.dart';
 
 class RegisterViewModel extends BaseViewModel {
   Logger log;
-  String _email = '';
-  String get email => _email;
-  String _password = '';
-  String get password => _password;
-  String _confirmPassword = '';
-  String get confirmPassword => _password;
+
   bool _isLoading = false;
   bool get isLoading => _isLoading;
 
   TextEditingController emailController = TextEditingController(text: '');
+  TextEditingController passwordController = TextEditingController(text: '');
+  TextEditingController confirmPasswordController =
+      TextEditingController(text: '');
 
-  AuthService _authService = locator<AuthService>();
   NavigationService _navigationService = locator<NavigationService>();
   DialogService _dialogService = locator<DialogService>();
   SnackbarService _snackbarService = locator<SnackbarService>();
+  FirebaseAuthenticationService _authenticationService =
+      locator<FirebaseAuthenticationService>();
 
   RegisterViewModel() {
     this.log = getLogger(this.runtimeType.toString());
-  }
-
-  void updateEmail(String value) {
-    _email = value;
-    notifyListeners();
-  }
-
-  void updatePassword(String value) {
-    _password = value;
-    notifyListeners();
-  }
-
-  void updateConfirmPassword(String value) {
-    _confirmPassword = value;
-    notifyListeners();
   }
 
   void toggleIsLodaing() {
@@ -50,40 +33,18 @@ class RegisterViewModel extends BaseViewModel {
     notifyListeners();
   }
 
-  // ! Make sure which one is better from AuthService or directly
-  // ? This is directly from FirebaseAuthenticationService() package.
-  void navigateToLogin() async {
-    _navigationService.back();
-  }
-
-  // ? old sign in from AuthService
-  // void signinWithEmail() {
-  //   _authService.signInWithEmail(email: email, password: password);
-  // }
-
-  void signupWithEmail() async {
-    if (_password == _confirmPassword) {
-      final result = await FirebaseAuthenticationService()
-          .createAccountWithEmail(
-              email: emailController.text, password: password);
-
-      print(emailController.text);
-      print(result.uid);
-    } else {
-      print('The password not Matched');
-    }
-  }
-
   void registerWithEmail() async {
-    if (_email == '' || _password == '' || _confirmPassword == '') {
+    if (emailController.text == '' ||
+        passwordController.text == '' ||
+        confirmPasswordController.text == '') {
       _snackbarService.showSnackbar(message: 'Please fill the required fields');
     } else {
-      if (_password != _confirmPassword) {
+      if (passwordController.text != confirmPasswordController.text) {
         _snackbarService.showSnackbar(message: 'Passwords does not match');
       } else {
         toggleIsLodaing();
-        final result = await FirebaseAuthenticationService()
-            .createAccountWithEmail(email: email, password: password);
+        final result = await _authenticationService.createAccountWithEmail(
+            email: emailController.text, password: passwordController.text);
 
         if (result.hasError) {
           toggleIsLodaing();
@@ -96,10 +57,7 @@ class RegisterViewModel extends BaseViewModel {
           toggleIsLodaing();
           print('Auth create account with email');
           print('Result uid: ${result.uid}');
-          _navigationService.pushNamedAndRemoveUntil(
-            todayViewRoute,
-            // arguments: {1: result.uid},
-          );
+          _navigationService.pushNamedAndRemoveUntil(homeViewRoute);
         }
       }
     }
