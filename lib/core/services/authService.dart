55b4cf1fa855d469ce7860c7af6d20150243bdf7
mukxdev <mukxdev@gmail.com>
2021-03-09 07:35:32 +0300

    Updated authService to be the main AuthService

--- lib/core/services/authService.dart
+++ lib/core/services/authService.dart
@@ -1,44 +1,46 @@
+import 'package:dotdo/core/locator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
 import 'package:stacked_firebase_auth/stacked_firebase_auth.dart';
 
 class AuthService {
   User _user;
   User get user => _user;
-  Future createNewAccount(
-      {@required String email, @required String password}) async {
-    final result = await FirebaseAuthenticationService().createAccountWithEmail(
-      email: email,
-      password: password,
-    );
-    if (result.hasError) {
-      print(result.errorMessage);
-      return result.errorMessage;
-    }
-    print('Auth create account with email');
-    print('Result uid: ${result.uid}');
-    return result;
-  }
 
-  Future signInWithEmail(
-      {@required String email, @required String password}) async {
-    final result = await FirebaseAuthenticationService().loginWithEmail(
-      email: email,
-      password: password,
-    );
-    if (result.hasError) {
-      print(result.errorMessage);
-      return result;
-      // return result.errorMessage;
-    }
-    print('Auth sign in with email');
-    print('Result uid: ${result.uid}');
-    return result;
-  }
+  final FirebaseAuthenticationService _firebaseAuthenticationService =
+      locator<FirebaseAuthenticationService>();
 
   final _firebaseAuth = FirebaseAuth.instance;
 
   Future getCurrentUser() async {
     _user = _firebaseAuth.currentUser;
+  }
+
+  Future<String> getCurrentUserId() async {
+    String _uid = _firebaseAuth.currentUser.uid;
+    return _uid;
+  }
+
+  Future<bool> get hasUser async {
+    // Future<bool> has = await _firebaseAuthenticationService.hasUser;
+    // return _firebaseAuthenticationService.hasUser;
+    bool hasUser = _firebaseAuth.currentUser != null;
+    return hasUser;
+  }
+
+  Future<FirebaseAuthenticationResult> signinWithEmail(
+      {String email, String password}) async {
+    return _firebaseAuthenticationService.loginWithEmail(
+        email: email, password: password);
+  }
+
+  // TODO: Implement using fullName
+  Future<FirebaseAuthenticationResult> registerWithEmail(
+      {String fullName, String email, String password}) async {
+    return _firebaseAuthenticationService.createAccountWithEmail(
+        email: email, password: password);
+  }
+
+  void logout() {
+    _firebaseAuthenticationService.logout();
   }
 }
