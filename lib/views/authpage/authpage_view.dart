f5c5077f35890dbc4d498fde898a5dae7f34a63f
mukxdev <mukxdev@gmail.com>
2021-03-09 07:38:27 +0300

    removed SmoothPageIndicator

--- lib/views/authpage/authpage_view.dart
+++ lib/views/authpage/authpage_view.dart
@@ -2,7 +2,6 @@ import 'package:dotdo/constant.dart';
 import 'package:dotdo/views/login/login_view.dart';
 import 'package:dotdo/views/register/register_view.dart';
 import 'package:flutter/material.dart';
-import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:stacked/stacked.dart';
 import 'authpage_view_model.dart';
 
@@ -27,29 +26,6 @@ class AuthpageView extends StatelessWidget {
                 navigateToIndex: () => viewModel.navigateToIndex(0),
               ),
             ],
-          ),
-          // * SmoothPageIndicator
-          bottomNavigationBar: Container(
-            width: double.infinity,
-            height: 80,
-            color: Theme.of(context).scaffoldBackgroundColor,
-            child: Column(
-              children: [
-                Center(
-                  child: SmoothPageIndicator(
-                    onDotClicked: (index) => viewModel.navigateToIndex(index),
-                    controller: viewModel.pageController,
-                    count: 2,
-                    effect: WormEffect(
-                      dotHeight: 15,
-                      dotWidth: 15,
-                      dotColor: Theme.of(context).primaryColor,
-                      activeDotColor: Theme.of(context).accentColor,
-                    ),
-                  ),
-                ),
-              ],
-            ),
           ),
         );
       },
