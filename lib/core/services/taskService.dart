793e7fea1ac7914cff2fc5e0cd12596cb1fb5f54
mukxdev <mukxdev@gmail.com>
2021-03-09 07:39:45 +0300

    Update taskService.dart
    Updated to ReactiveViewModel

--- lib/core/services/taskService.dart
+++ lib/core/services/taskService.dart
@@ -1,10 +1,25 @@
 import 'package:dotdo/core/models/task.dart';
 import 'package:flutter/material.dart';
-import 'package:uuid/uuid.dart';
+import 'package:stacked/stacked.dart';
+// import 'package:uuid/uuid.dart';
+import 'package:observable_ish/observable_ish.dart';
 
-class TaskService {
+class TaskService with ReactiveServiceMixin {
   // This will generate a uniqe key use .v1() or .v4()
-  var uuid = Uuid();
+  // var uuid = Uuid();
+  final todayTaskListKey = GlobalKey<AnimatedListState>();
+
+  RxList<Task> _rxList = RxList();
+  RxList<Task> get rxTaskList => _rxList;
+
+  TaskService() {
+    listenToReactiveValues([_rxList]);
+  }
+
+  void updateRxListFromFirebase() {
+    _rxList.assignAll(_taskList);
+    // notifyListeners();
+  }
 
   List<Task> get taskList => _taskList;
   List<Task> get doneTaskList =>
@@ -13,11 +28,22 @@ class TaskService {
       _taskList.where((task) => task.checked == false).toList();
 
   void removeTask(id) {
-    taskList.removeWhere((task) => task.id == id);
+    // taskList.removeWhere((task) => task.id == id);
+    _rxList.removeWhere((task) => task.id == id);
+    // notifyListeners();
   }
 
+  // * This will update the today Task list
   void addTask(Task task) {
-    taskList.add(task);
+    // taskList.add(task);
+    _rxList.add(task);
+    todayTaskListKey.currentState.insertItem(0);
+  }
+
+  void toggleCheckedTask(Task task) {
+    bool checked = task.checked;
+    Task newTask = task.copyWith(checked: !checked);
+    _rxList.add(newTask);
   }
 
   // TODO: implement retreving (Tasks List) data from user.
