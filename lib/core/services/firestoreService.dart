6972a12bd4588f6df250eaf0565f29ff88fba9bb
Hussain ali <64625319+HSALI18@users.noreply.github.com>
2021-03-15 21:17:41 +0300

    added a comment

--- lib/core/services/firestoreService.dart
+++ lib/core/services/firestoreService.dart
@@ -3,7 +3,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class FirestoreService {
   // * References
   CollectionReference users = FirebaseFirestore.instance.collection('users');
-
+  // ? methods
   Future<void> addUser({String uid, String fullName, String email}) {
     // Call the user's CollectionReference to add a new user
     return users
